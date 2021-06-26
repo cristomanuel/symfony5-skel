b02f9d5cb22fa9accd48b060961146a1671820d9
cristomanuel <cristomanuel@gmail.com>
2021-06-26 19:09:29 +0100

    namespaces

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Cmas\MyApp;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
